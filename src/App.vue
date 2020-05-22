e8bd5cdbe38b38eaa0fba236685af710d4b3cbbf
yangxiaoying <yangxiaoying@sinorock.net>
2020-05-22 14:48:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="upload-writ-content">
+    <section v-if="imgArr.length !== 0" class="writ-wrapper">
+      <div>
+        <div v-for="(img, index) in imgArr" :key="index">
+          <div class="writ-show">
+            <img width="200px" :src="img.path" alt="" class="img-style" />
+            <div class="writ-oper">
+              <i @click="del(index)" class="el-icon-delete public del-right"></i>
+              <span class="down-file-name" :title="img.name">{{ img.name }}</span>
+            </div>
+          </div>
+        </div>
+        </div>
+    </section>
+    <section v-show="imgArr.length === 0" class="buttons-wrapper">
+      <div class="upload-proof-tips" @click="select">
+          选择
+        <i slot="default" class="iconfont iconshangchuan"></i>
+      </div>
+      <input style="display: none" id="file-uploader" type="file" name="file" multiple="multiple" />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayImg',
+  data() {
+    return {
+      imgArr: []
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      let uploader = document.getElementById('file-uploader')
+      uploader.addEventListener('change', event => {
+        if (!this.validate(uploader)) return
+        Array.prototype.forEach.call(uploader.files, file => {
+          let img = { path: '', name: file.name, file: void 0 }
+          let blobFile = new File([file], file.name, { type: 'image/jpg' })
+          img.file = blobFile
+          img.path = URL.createObjectURL(blobFile)
+          this.imgArr.push(img)
+        })
+        this.$emit('afterSelect', this.imgArr)
+      })
+    },
+    getMessage(type, message) {
+      this.$message({
+        type: type,
+        message: message
+      })
+    },
+    // 校验选择的上传内容
+    validate(uploader) {
+      let typeList = 'image/jpeg/image/jpg/image/png'
+      typeList += typeList.toUpperCase()
+      let res = true
+      Array.prototype.forEach.call(uploader.files, file => {
+        if (typeList.indexOf(file.name.substring(file.name.lastIndexOf('.') + 1)) < 0) {
+          alert('请选择图片')
+          res = false
+        }
+      })
+      return res
+    },
+    // 触发file-uploader的click事件
+    select() {
+      let uploader = document.getElementById('file-uploader')
+      uploader.click()
+    },
+    // 删除图片
+    del(ind) {
+      this.imgArr.splice(ind, 1)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.upload-writ-content {
+  max-height: 70vh;
+  position: relative;
+  padding: 10px;
+  .buttons-wrapper {
+    display: flex;
+    justify-content: space-around;
+    width: 50%;
+    margin: 0 auto 30px;
+    padding-bottom: 10px;
+    .upload-proof-tips {
+      width: 100px;
+      height: 100px;
+      text-align: center;
+      line-height: 100px;
+      background: #eef5f9;
+      border: 1px dashed #c0ccda;
+      border-radius: 6px;
+      cursor: pointer;
+      &:hover {
+        border: 1px dashed #0099ff;
+      }
+      .iconshangchuan {
+        color: #0099ff;
+      }
+    }
+    i {
+      margin-bottom: 17px;
+      vertical-align: bottom;
+    }
+  }
+  .writ-wrapper {
+    max-height: calc(70vh - 40px);
+    overflow-y: auto;
+    overflow-x: hidden;
+    > div {
+      /* display: flex;
+      width: auto;
+      justify-content: flex-start;
+      flex-wrap: wrap; */
+      display: grid;
+      width: 100%;
+      grid-template-columns: repeat(4, 25%);
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      padding: 10px;
+      margin-bottom: 30px;
+    }
+  }
+  .writ-show {
+    width: 200px !important;
+    height: 146px;
+    position: relative;
+    overflow: hidden;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    border-radius: 6px;
+    .img-style {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    .writ-oper {
+      visibility: hidden;
+      width: 220px;
+      height: 146px;
+    }
+    &:hover {
+      .writ-oper {
+        visibility: visible;
+        position: relative;
+        background-color: rgba(0, 0, 0, 0.5);
+        font-size: 30px;
+        color: #ffffff;
+        .public {
+          position: absolute;
+          top: 40%;
+          &:hover {
+            cursor: pointer;
+          }
+        }
+        .del-right {
+          right: 20%;
+        }
+        .down-file-name {
+          display: inline-block;
+          max-height: 60px;
+          text-align: center;
+          width: 100%;
+          font-size: 14px;
+          position: absolute;
+          bottom: 5px;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          overflow: hidden;
+        }
+      }
+    }
+  }
+}
+</style>
